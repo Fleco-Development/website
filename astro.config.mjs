6044c049d927b4eddb3a42d7e760e3cdccce24bc
Shane Caffoe <shane@scaffoe.com>
2023-11-02 03:45:15 +0000

    remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/website',
 	integrations: [starlight({
 		title: 'Fleco',
 		logo: {
